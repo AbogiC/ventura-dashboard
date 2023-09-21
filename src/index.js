f1d7b03cb127e1f44a37ebd174ffc071df549895
VENTURA\ventu <venturaabedbogichrist314@gmail.com>
2023-09-21 18:08:41 +0700

    add example dashboard

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
 
-import App from "./App";
+import AdminLayout from "layouts/Admin/Admin.js";
 
 import "assets/scss/black-dashboard-react.scss";
 import "assets/demo/demo.css";
@@ -19,7 +19,7 @@ root.render(
     <BackgroundColorWrapper>
       <BrowserRouter>
         <Routes>
-          <Route path="/admin/*" element={<App />} />
+          <Route path="/admin/*" element={<AdminLayout />} />
           <Route
             path="*"
             element={<Navigate to="/admin/dashboard" replace />}
